4e065a627d26098c6d942be9ccbde260cea5debf
iocheredko <iocheredko@veritone.com>
2022-02-09 15:51:03 -0800

    Fixed NodeJS version to use cacheURI instead of a file.

--- javascript/hello-world/index.js
+++ javascript/hello-world/index.js
@@ -3,6 +3,7 @@
 const KeywordExtraction = require("./keyword-extraction.js"),
     express = require('express'),
     multer = require('multer'),
+    fetch = require('node-fetch-commonjs'),
     upload = multer({storage: multer.memoryStorage()}),
     app = express();
 
@@ -18,10 +19,16 @@ app.get('/ready', (req, res) => {
 // PROCESS WEBHOOK
 app.post('/process', chunkUpload, async (req, res)=>{
     try {
-        let buffer = req.file.buffer.toString();
+        const cacheUri = req.body.cacheURI;
+        console.log(`Reading from ${cacheUri}`);
+
+        const response = await fetch(cacheUri);
+        const buffer = await response.text();
+
         let output = KeywordExtraction.getOutput( buffer, null );
         return res.status(200).send( output );
     } catch (error) {
+        console.log(error);
         return res.status(500).send(error);
     }
 });
